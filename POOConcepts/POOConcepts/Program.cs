56b66414df632eb950e3f8f03e80c9be30d646e8
Antonio Garrido <agg@etkon.es>
2022-02-13 08:18:19 +0100

    Finish date class

---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using POOConcepts;
+
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29)); ;
+    Console.WriteLine(new Date(1974, 9, 23)); ;
+    Console.WriteLine(new Date(1985, 11, 31)); ;
+
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
+
+
